292d91f72a47af5cd6d2805a122f2f0e295b88c2
Matt Dancho <mdancho@gmail.com>
2024-04-26 14:56:26 -0400

    revert - pull #3 didn't work with grid_random

--- 04_fraud_detection/02_fraud_detection.R
+++ 04_fraud_detection/02_fraud_detection.R
@@ -1,6 +1,9 @@
 # BUSINESS PROBLEMS THAT CAN BE SOLVED WITH DATA SCIENCE
 # PROJECT 4: FRAUD DETECTION
 
+# BUSINESS PROBLEMS THAT CAN BE SOLVED WITH DATA SCIENCE
+# PROJECT 4: FRAUD DETECTION
+
 library(tidyverse)
 library(tidymodels)
 library(xgboost)
@@ -8,85 +11,73 @@ library(xgboost)
 # Generate synthetic data
 set.seed(0)
 data_size <- 1000
-transaction_types <- sample(c("type1", "type2", "type3"), size = data_size, replace = TRUE)
+transaction_types <- sample(c("type1", "type2", "type3"), size = data_size, replace = TRUE) # nolint
 transaction_amounts <- rexp(n = data_size, rate = 1 / 200)
 age_of_account_days <- rnorm(n = data_size, mean = 365, sd = 100)
-interaction_term <- transaction_amounts * age_of_account_days  # New feature: Interaction term
 fraudulent <- vector("numeric", length = data_size)
 
 for (i in 1:data_size) {
-  if (transaction_types[i] == "type1" && transaction_amounts[i] > 100 && age_of_account_days[i] < 365) {
-    fraudulent[i] <- sample(c(0, 1), size = 1, prob = c(0.1, 0.9))
-  } else {
-    fraudulent[i] <- sample(c(0, 1), size = 1, prob = c(0.99, 0.01))
-  }
+    if (transaction_types[i] == "type1" && transaction_amounts[i] > 100 && age_of_account_days[i] < 365) {
+        fraudulent[i] <- sample(c(0, 1), size = 1, prob = c(0.1, 0.9)) # 90% chance of being fraudulent
+    } else {
+        fraudulent[i] <- sample(c(0, 1), size = 1, prob = c(0.99, 0.01)) # 1% chance as normal
+    }
 }
 
 df <- tibble(
-  transaction_amount = transaction_amounts,
-  transaction_type = transaction_types,
-  age_of_account_days = age_of_account_days,
-  interaction_term = interaction_term,
-  fraudulent = as.factor(fraudulent)
-)
+    transaction_amount = transaction_amounts,
+    transaction_type = transaction_types,
+    age_of_account_days = age_of_account_days,
+    fraudulent = as.factor(fraudulent)
+) %>% rowid_to_column()
+
+df_untransformed <- df
 
 # Data preparation
 df <- df %>%
-  mutate(transaction_type = as.numeric(as.factor(transaction_type)))
+    mutate(transaction_type = as.numeric(as.factor(transaction_type))) # Encode categorical data as numeric
 
-# Split the data
+# Splitting the data
 set.seed(0)
 split <- initial_split(df, prop = 0.8)
 train_data <- training(split)
 test_data <- testing(split)
 
-# Model specification with hyperparameter tuning
-xgb_spec <- boost_tree(
-  trees = tune(),
-  tree_depth = tune(),
-  min_n = tune(),
-  loss_reduction = tune(),
-  sample_size = tune(),
-  mtry = tune(),
-  learn_rate = tune()
-) %>%
-  set_engine("xgboost", eval_metric = "logloss") %>%
-  set_mode("classification")
+# XGBoost Model Setup
+xgb_spec <- boost_tree(trees = 50, tree_depth = 6, min_n = 10) %>%
+    set_engine("xgboost", eval_metric = "logloss") %>%
+    set_mode("classification")
 
-# Workflow setup
-xgb_wf <- workflow() %>%
-  add_model(xgb_spec) %>%
-  add_formula(fraudulent ~ .)
+# Fit the Model
+xgb_fit <- xgb_spec %>%
+    fit(fraudulent ~ . - rowid, data = train_data)
 
-# Tuning parameters
-xgb_grid <- grid_random(
-  trees = c(50, 100),
-  tree_depth = c(6, 10),
-  min_n = c(10, 20),
-  loss_reduction = c(0, 0.01),
-  sample_size = c(0.8, 1.0),
-  mtry = c(2, 3),
-  learn_rate = c(0.01, 0.1),
-  size = 30
-)
+# Make Predictions
+test_results <- test_data %>%
+    select(-fraudulent) %>%
+    predict(xgb_fit, new_data = ., type = "prob") %>%
+    mutate(predict_class = as.numeric(.pred_1 > 0.5) %>% as.factor()) %>%
+    bind_cols(
+        df_untransformed %>% filter(rowid %in% test_data$rowid)
+    )
 
-# Tune the model
-set.seed(0)
-xgb_results <- tune_grid(
-  xgb_wf,
-  resamples = vfold_cv(train_data, v = 5),
-  grid = xgb_grid
-)
+# Evaluate the Model
+test_results %>%
+    select(fraudulent, predict_class) %>%
+    yardstick::metrics(truth = fraudulent, estimate = predict_class)
 
-# Best model selection
-best_params <- select_best(xgb_results, "logloss")
+# Confusion Matrix
+conf_mat(test_results, truth = fraudulent, estimate = predict_class)
 
-# Final model training
-final_xgb_model <- finalize_workflow(
-  xgb_wf,
-  best_params
-) %>%
-  last_fit(split)
-
-# Results
-final_xgb_model
+# Visualization
+test_results %>%
+    ggplot(aes(x = transaction_amount, fill = as.factor(fraudulent))) +
+    geom_histogram(position = "identity", alpha = 0.5, bins = 30) +
+    scale_fill_manual(values = c("gray", "red"), labels = c("Legitimate", "Fraudulent")) +
+    labs(
+        title = "Transaction Amount Distribution",
+        x = "Transaction Amount",
+        y = "Frequency",
+        fill = "Transaction Type"
+    ) +
+    theme_minimal()
